869a104ad506a06e94eda388e2b24f617f921c3c
Rupesh Bhochhibhoya <rbhochhibhoya@telogical.com>
2012-04-03 15:07:41 -0500

    wip

---
+++ app/assets/javascripts/grocery.js
@@ -0,0 +1,50 @@
+/**
+ * Created by JetBrains RubyMine.
+ * User: RBhochhibhoya
+ * Date: 4/2/12
+ * Time: 9:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$(function () {
+    //Model
+    window.Grocery = Backbone.Model.extend({
+
+    });
+
+    //Collection
+    window.GroceryList = Backbone.Collection.extend({
+        model:Grocery,
+        url:'/groceries'
+    });
+    window.Groceries = new GroceryList;
+
+    window.GroceryView = Backbone.View.extend({
+        el:$("#grocery-app"),
+        events:{
+            "click .add-item":"addOneItem",
+            "click .check-item":"toggleBought",
+            "click .destory-item a":"deleteItem"
+        },
+
+        addOneItem:function () {
+            if ($(".new-item").val()) {
+                $(".grocery-list").append("<li><input class='check-item' type='checkbox'><div class='each-item'>" + $(".new-item").val() + "</div><div class='destory-item'><a href='#'>[X]</a></div></li>");
+            }
+            else
+                alert("Enter an item first");
+        },
+        deleteItem:function () {
+
+                   alert("Your item will be deleted");
+        },
+        toggleBought:function () {
+            alert("Got it");
+        }
+
+
+    });
+    window.App = new GroceryView;
+});
+
